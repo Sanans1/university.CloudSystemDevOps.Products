204c6786948205dd44c1fb019e1b993f1a07291a
Lewis Carter <lewis.r.d.carter@outlook.com>
2019-11-07 15:35:03 +0000

    Add project files.

---
+++ DevOps.Products.Website/Services/Implementations/ProductFacadeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevOps.Products.Website.Services.Implementations
+{
+    public class ProductFacadeService
+    {
+    }
+}
---
+++ DevOps.Products.Website/Services/Implementations/ProductListService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevOps.Products.Website.Models.ViewModels;
+
+namespace DevOps.Products.Website.Services.Implementations
+{
+    public class ProductListService
+    {
+        public async List<ProductListViewModel> GetProductListViewModelsAsync()
+        {
+            
+        }
+    }
+}
