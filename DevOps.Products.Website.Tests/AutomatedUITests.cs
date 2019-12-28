eae40df25ca67b16cb40f2cf47269964c0dc58c7
Lewis carter <lewiscarter998@gmail.com>
2019-12-28 21:00:12 +0000

    Totally commented out the test so it doesn't run.

--- DevOps.Products.Website.Tests/AutomatedUITests.cs
+++ DevOps.Products.Website.Tests/AutomatedUITests.cs
@@ -22,14 +22,14 @@ namespace DevOps.Products.Website.Tests
             _driver.Dispose();
         }
 
-        [Test]
-        public async Task ProductListPage_WhenExecuted_ReturnsProductListPage()
-        {
-            //_driver.Navigate().GoToUrl("localhost:63651");
+        //[Test]
+        //public async Task ProductListPage_WhenExecuted_ReturnsProductListPage()
+        //{
+        //    _driver.Navigate().GoToUrl("localhost:63651");
 
-            //IWebElement header = _driver.FindElement(By.Id("TitleHeader"));
-            //Assert.IsTrue(header.Displayed);
-            //Assert.AreEqual("Products", header.Text);
-        }
+        //    IWebElement header = _driver.FindElement(By.Id("TitleHeader"));
+        //    Assert.IsTrue(header.Displayed);
+        //    Assert.AreEqual("Products", header.Text);
+        //}
     }
 }
